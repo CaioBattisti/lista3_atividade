1598c8c8c6fbe74711ba6547792997eb1589c73d
CaioBattisti <cluizbattisti@gmail.com>
2025-02-14 20:23:02 -0300

    fazer

--- atividade1_CaioLuizBattisti.py
+++ atividade1_CaioLuizBattisti.py
@@ -2,8 +2,8 @@
 #O grau de aceitação de risco e o valor a ser investido. O grau de aceitação de risco deve ser lido no teclado na forma BX ou AL 
 #se for fornecido algo diferente disso o programa deve mostrar uma mensagem indicando que foi fornecido dado invalido.
 #Para o valor dever ser numero real.
-aceitacao_risco = input("qual sua aceitação de risco, BX ou AL? ")
+aceitacao_risco =real input("qual sua aceitação de risco, BX ou AL? ")
 if aceitacao_risco == "bx":
     investir = input("quando deseja investir? ")
-elif investir < 100:
+if investir < 100:
     print("voce deve abrir uma poupança.")
