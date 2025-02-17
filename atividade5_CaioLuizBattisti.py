2a9bdac93f43709bd46e7fcc3e986ea73136651a
CaioBattisti <cluizbattisti@gmail.com>
2025-02-17 20:36:42 -0300

    finalizado 5

---
+++ atividade5_CaioLuizBattisti.py
@@ -0,0 +1,7 @@
+preco_custo =float(input("Digite o preço de custo do produto: "))
+if preco_custo <=100:
+    margem = 0.45
+else:
+    margem = 0.35
+preco_venda =preco_custo * (1+margem)
+print("O preço de venda do produto é:R$",preco_venda)
