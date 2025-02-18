ae2c1a7a4ddbc8b755044b27e450ce3ed3d195e2
CaioBattisti <cluizbattisti@gmail.com>
2025-02-17 22:08:31 -0300

    atualização

--- atividade1_CaioLuizBattisti.py
+++ atividade1_CaioLuizBattisti.py
@@ -1,19 +1,20 @@
 #Escreva um programa que forneça um tipo de aplicação financeira adequada ao um investidor a partir de dois dados fornecidos:
-#O grau de aceitação de risco e o valor a ser investido. O grau de aceitação de risco deve ser lido no teclado na forma BX ou AL 
+#O grau de aceitação de risco e o valor a ser investido. O grau de aceitação de risco deve ser lido no teclado na forma BX(baixo valor) ou AL(auto valor) 
 #se for fornecido algo diferente disso o programa deve mostrar uma mensagem indicando que foi fornecido dado invalido.
 #Para o valor dever ser numero real.
-aceitacao_risco = input("qual sua aceitação de risco, BX ou AL? ")
-if aceitacao_risco == "bx":
-    renda =float(input("quando vc quer depositar? "))
-if renda < (1000.00):
+#bx:
+bxrenda =float(input("Quanto voce quer depositar? "))
+aceitacao_risco = input("qual sua aceitação de risco, BX ou AL? ").strip().lower()
+if aceitacao_risco == "bx" and bxrenda < 1000.00:
     print("voce deve abrir uma poupança.")
-if renda >= (1000.00):
+if bxrenda > 1000.00:
     print("voce deve abrir uma renda fixa.")
-elif aceitacao_risco == "al":
-    renda2 =float(input("quanto vc quer depositar? "))
-    if renda2 <(1000.00):
-        print("voce deve investir em bitcoin. ")
-    if renda2 >= (1000.00):
-        print("voce deve abrir uma ação. ")
-#else:
-    #print("dado informado invalido!")
+#al:    
+if aceitacao_risco == "al":
+    alrenda =float(input("Quanto voce quer depositar? "))
+    if alrenda < 1000.00:
+        print("voce deve investir em bitcoin.")
+    if alrenda > 1000.00:
+        print("voce deve abrir uma ação")
+    else:
+        print("dado invalido, tente novamente.")
